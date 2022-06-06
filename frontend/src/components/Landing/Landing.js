624db4d0cd9bf5903e0e750b8e80b94e6af85a1e
Param <param23403@gmail.com>
2022-06-06 17:18:13 -0400

    123

--- frontend/src/components/Landing/Landing.js
+++ frontend/src/components/Landing/Landing.js
@@ -1,8 +1,24 @@
 import Navbar from '../Navbar/Navbar.js'
+import {Button, Box, Grid} from '@mui/material'
+import { margin,positions } from '@mui/system';
+
 const Landing=()=>{
+    const linkStyle = {
+        margin: "1rem",
+        textDecoration: "none",
+        color: 'black',
+        backgroundColor: "#5BAFFF",
+    };
     return(
         <>
-    <Navbar ispage={[false, false, false]}/>
+        <Grid container spacing={2} alignItems ="right">
+            <Grid container item sx={9} justifyContext="flex-start">
+                <Button variant='contained'style={linkStyle}>Login</Button>
+                <Button variant='contained'style={linkStyle}>Sign Up</Button>
+            </Grid>
+        </Grid>
+
+
     <h1>This is the Landing Page</h1>
     </>
    ) }
