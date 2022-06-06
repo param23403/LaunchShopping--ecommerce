38033937dccc5e72c3f2c3f932769bfe2979e0b4
Matthew Haid <wsd6vn@virginia.edu>
2022-06-06 15:47:57 -0400

    usercreation functions

--- api/routes/shopping.js
+++ api/routes/shopping.js
@@ -4,9 +4,29 @@ var fetch = require("node-fetch");
 const db = require("./firebase");
 
 const {
-  addDoc,
+  setDoc, getDocs,
 } = require("firebase/firestore");
+const { async } = require("@firebase/util");
 
+router.post("/userCreation", async (req, res, next) => {
+    await setDoc(doc(db, "Users", req.query.id), {
+        username: req.query.id,
+        password: req.body.password,
+        birthday: req.body.birthday,
+        creditCardNumber: req.body.creditCardNumber,
+        address: req.body.address,
+        phoneNumber: req.body.phoneNumber
+    })
 
+});
+
+router.get("/usernames", async (req, res, next) => {
+    const users = [];
+    const docs = await getDocs(collection(db, "Users"));
+    docs.forEach((user) =>
+        users.push({ id: user.id })
+    );
+    res.json({ result: users });
+});
 
 module.exports = router;
