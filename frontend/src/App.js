f703669688d517c4c1c03baacb38e1dab12bc786
yangjus <justiny_2013@yahoo.com>
2022-06-06 17:18:08 -0400

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Error from './components/Error.js';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home.js';
+import React from 'react';
+
+document.title = "shopCrunch";
+document.body.style = 'background: #D6FFD6;';
 
 function App() {
   return (
     <div className="App">
-        
+      <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="*" element={<Error />}/>
+    </Routes>
     </div>
   );
 }
