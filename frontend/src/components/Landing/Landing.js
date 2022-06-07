8bcb1eac4d3b9c18fd4c3bda056bb36616401d58
yangjus <justiny_2013@yahoo.com>
2022-06-07 11:24:25 -0400

    landing page

--- frontend/src/components/Landing/Landing.js
+++ frontend/src/components/Landing/Landing.js
@@ -1,25 +1,50 @@
-import {Button, Grid} from '@mui/material'
+import {Button, Box, Typography} from '@mui/material'
 import React from 'react';
+import logo from "./logo.png";
+import background from "./background_overlay.jpg";
+import { Link } from "react-router-dom";
 
-const Landing=()=>{
+const Landing = () => {
     const linkStyle = {
         margin: "1rem",
         textDecoration: "none",
-        color: 'black',
+        color: 'white',
         backgroundColor: "#5BAFFF",
     };
+
+    const imageOverlay = {
+        backgroundImage: `url("${background}"), linear-gradient(#D6FFD6, #5BAFFF)`,
+        backgroundBlendMode: "multiply",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
+    }    
+
     return(
-        <>
-        <Grid container spacing={2} alignItems ="right">
-            <Grid container item sx={9} justifyContext="flex-start">
-                <Button variant='contained'style={linkStyle}>Login</Button>
-                <Button variant='contained'style={linkStyle}>Sign Up</Button>
-            </Grid>
-        </Grid>
-
-
-    <h1>This is the Landing Page</h1>
-    </>
-   ) }
+        <div id="root" style={imageOverlay}>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', borderRadius: 1}}>
+                    <img src={logo} style={{ maxWidth: 175, maxHeight: 50, padding: 15}} alt="logo"></img>
+                    <Box>
+                        <Link to="/login">
+                            <Button variant='contained'style={linkStyle}>Login</Button>
+                        </Link>
+                        <Link to="/signup">
+                            <Button variant='contained'style={linkStyle}>Sign Up</Button>
+                        </Link>
+                    </Box>
+            </Box>
+            <Box sx={{ margin:"auto", marginTop: "30vh", display:"flex", justifyContent:"center", alignItems:"center", maxHeight:"40vh", 
+                border: 4, borderColor: 'white', maxWidth:"30vw", flexDirection:"column"}}>
+                <Typography style={{ fontSize: 45, color: 'white', fontFamily: 'georgia'}}>Online Shopping</Typography>
+                <Typography style={{ fontSize: 45, color: 'white', fontFamily: 'georgia'}}> Done Right.</Typography>
+            <Link to="/shopping">
+                <Button variant='contained' size='large' style={{ margin: "1rem", borderRadius: 30, 
+                    fontFamily: 'georgia', fontSize: 20, textTransform: 'none' }}>Shop Now</Button>
+            </Link>
+            </Box>
+        </div>
+    ) 
+};
     
-    export default Landing
+export default Landing
