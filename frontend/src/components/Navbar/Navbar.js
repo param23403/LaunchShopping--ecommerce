d1bd053034ed5f481988b61dd344e6ab96c2ac10
yangjus <justiny_2013@yahoo.com>
2022-06-07 16:48:11 -0400

    navbar changes

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,9 +1,8 @@
-import {React} from "react";
+import {React, useState} from "react";
 import { Link } from "react-router-dom";
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { useNavigate } from "react-router-dom";
 import './Navbar.css'
@@ -30,19 +29,33 @@ const Navbar = (props) => {
     const linkStyle = {
         margin: "1rem",
         textDecoration: "none",
-        color: 'white'
+        color: 'white',
     };
+
+    const [isPage, setPage] = useState(props.ispage);
+
+    const landonclick=()=>{
+        navigate('/')
+    }
     
     return (
         <>
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar style={{backgroundColor: "#5BAFFF"}}>
-                <img src={logo} style={{ maxWidth: 175, maxHeight: 50, padding: 15}} alt="logo"></img>
-                        <Link to='/shopping'style={linkStyle}><Button color="inherit"  onClick={shoppingOnClick}>Shop</Button></Link>
-                        <Link to='/cart'style={linkStyle}><Button color="inherit" onClick={cartOnClick}>Cart</Button></Link>
-                        <Link to ='/account'style={linkStyle}><Button color="inherit" onClick={accountOnClick}>Account</Button></Link>
-                        <Link to='/'style={linkStyle}><Button color="inherit" onClick={logoutOnClick}>Logout</Button></Link>
+                <img src={logo} onClick={landonclick} style={{ maxWidth: 175, maxHeight: 50, padding: 15}} alt="logo"></img>
+                        <Link to='/shopping'style={linkStyle}>
+                            <Button color="inherit" style={{backgroundColor: isPage[0] ? "#1F73C3" : "#5BAFFF"}} onClick={shoppingOnClick}>Shop</Button>
+                        </Link>
+                        <Link to='/cart'style={linkStyle}>
+                            <Button color="inherit" style={{backgroundColor: isPage[1] ? "#1F73C3" : "#5BAFFF"}} onClick={cartOnClick}>Cart</Button>
+                        </Link>
+                        <Link to='/account'style={linkStyle}>
+                            <Button color="inherit" style={{backgroundColor: isPage[2] ? "#1F73C3" : "#5BAFFF"}} onClick={accountOnClick}>Account</Button>
+                        </Link>
+                        <Link to='/'style={linkStyle}>
+                            <Button color="inherit" onClick={logoutOnClick}>Logout</Button>
+                        </Link>
                 </Toolbar>
             </AppBar>
         </Box>
