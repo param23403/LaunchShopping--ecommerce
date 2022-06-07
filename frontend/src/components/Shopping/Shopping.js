988ebe2215aa20841c520107a901829b918a3e42
Matthew Haid <wsd6vn@virginia.edu>
2022-06-07 16:32:45 -0400

    shopping

--- frontend/src/components/Shopping/Shopping.js
+++ frontend/src/components/Shopping/Shopping.js
@@ -1,11 +1,71 @@
-import Navbar from "../Navbar/Navbar"
+import Navbar from "../Navbar/Navbar";
+import { Grid, InputBase, IconButton, Card, Typography } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import React from "react";
+import jacket from "./jacket.png";
 
-const Homepage=()=>{
-    return(
-      <>
-      <Navbar ispage={[true, false, false]}/>
-    <h1>This is the Shopping Page</h1>
+const Homepage = () => {
+  return (
+    <>
+      <Navbar ispage={[true, false, false]} />
+      <Grid container justifyContent="center" marginTop="1%">
+        <Grid item xs={12}>
+          <Card sx={{ width: "15%", marginLeft: "42.5%" }}>
+            <InputBase
+              sx={{ ml: 1, flex: 1, width: "81%" }}
+              placeholder="Search Products"
+              inputProps={{ "aria-label": "search products" }}
+            />
+            <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+              <SearchIcon />
+            </IconButton>
+          </Card>
+        </Grid>
+        <Grid item xs={2}></Grid>
+        <Grid item xs={8}>
+          <Grid container justifyContent={"center"}>
+            <Grid item xs={3}>
+              <Card margin="1%">
+                <img src={jacket} alt='jacket' height="402px" width="302px" margin="auto"/>
+                <Typography variant="p">
+                  Designer Solid Brown Front Jacket
+                </Typography>
+                <Typography variant="p"><strong>US$59.99</strong></Typography>
+              </Card>
+            </Grid>
+            <Grid item xs={3}>
+            <Card>
+                <img src={jacket} alt='jacket' height="402px" width="302px" margin="auto"/>
+                <Typography variant="p">
+                  Designer Solid Brown Front Drop Straight Jacket
+                </Typography>
+                <Typography variant="p"><strong>US$59.99</strong></Typography>
+              </Card>
+            </Grid>
+            <Grid item xs={3}>
+            <Card>
+                <img src={jacket} alt='jacket' height="402px" width="302px" margin="auto"/>
+                <Typography variant="p">
+                  Designer Solid Brown Front Drop Straight Jacket
+                </Typography>
+                <Typography variant="p"><strong>US$59.99</strong></Typography>
+              </Card>
+            </Grid>
+            <Grid item xs={3}>
+            <Card>
+                <img src={jacket} alt='jacket' height="402px" width="302px" margin="auto"/>
+                <Typography variant="p">
+                  Designer Solid Brown Front Drop Straight Jacket
+                </Typography>
+                <Typography variant="p"><strong>US$59.99</strong></Typography>
+              </Card>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={2}></Grid>
+      </Grid>
     </>
-  )  }
-    
-    export default Homepage
+  );
+};
+
+export default Homepage;
--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,9 +1,31 @@
+import { Grid, InputBase, IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 import React from 'react';
 
 const Home = () => {
 
     return (
-        <h1>This is the landing page.</h1>
+        <Grid container>
+            <Grid item xs={12}>
+            <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Search Google Maps"
+        inputProps={{ 'aria-label': 'search google maps' }}
+      />
+      <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+            </Grid>
+            <Grid item xs={2}>
+
+            </Grid>
+            <Grid item xs={8}>
+
+            </Grid>
+            <Grid item xs={2}>
+
+            </Grid>
+        </Grid>
     );
 };
 
