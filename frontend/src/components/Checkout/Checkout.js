e992926a2912011ca0ce7e1295d8888a0571caf3
Param <param23403@gmail.com>
2022-06-10 11:05:54 -0400

    qwf

--- frontend/src/components/Checkout/Checkout.js
+++ frontend/src/components/Checkout/Checkout.js
@@ -14,7 +14,7 @@ const Checkout=()=>{
             <h5>$20.00</h5>
             </div>
         </div>
-        <form action="http://localhost:4242/create-checkout-session" method="POST">
+        <form action="https://shopcrunch.herokuapp.com:4242/create-checkout-session" method="POST">
             <button type="submit">
             Checkout
             </button>
