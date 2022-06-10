99a2d44d59c8aeeb2c4ea8ffa31bb95e8e21440f
yangjus <justiny_2013@yahoo.com>
2022-06-10 11:30:35 -0400

    changes

--- frontend/src/components/Account/Account.js
+++ frontend/src/components/Account/Account.js
@@ -1,18 +1,37 @@
-import React, {useEffect, useContext, useState} from "react";
+import {useEffect, useContext, useState} from "react";
 import "./Account.css";
 import Navbar from "../Navbar/Navbar";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../contexts/UserContext";
-import {Button, Typography,Container,Grid} from '@mui/material'
-
+import {Box, Paper, Button, Dialog,Typography,Grid, DialogTitle, DialogContent, TextField} from '@mui/material'
+import ClearIcon from '@mui/icons-material/Clear';
+import EditIcon from '@mui/icons-material/Edit';
+import { sizing } from '@mui/system';
 
 const Account=()=>{
     const user = useContext(UserContext);
     const [birthday,setBirthday] = useState()
     const [address,setAddress] = useState()
     const[number,setNumber] = useState()
-    const[cc,setCC] = useState()
+    const[username,setUsername] = useState()
 
+    const[cc,setCC] = useState()
+    const[passmodel,setPassModel] = useState()
+    const[bdaymodel,setbdayModel] = useState()
+    const[nummodel,setnumModel] = useState()
+    const[addmodel,setaddModel] = useState()
+    const[usernamemodel,setUserModel] = useState()
+
+    const[curpass, setCurPass] = useState()
+    const[newpass, setNewPass] = useState()
+    const[newconpass, setNewConPass] = useState()
+    const[fetchedpass,setFetchedpass] = useState()
+    const[errormodel,seterrorModel] = useState()
+    const[passerror,setpasserror] = useState()
+    const[newbday,setNewBday]=useState()
+    const[newadd,setNewAdd]=useState()
+    const[newnum,setNewNum]=useState()
+    const[newusername,setNewUserName]=useState()
     let navigate = useNavigate();
     
     useEffect(() => {
@@ -22,42 +41,217 @@ const Account=()=>{
     },[]);
    
     useEffect(() => {
+      fetch("/shopping/usernames")
+      .then((res) => res.json())
+      .then((data)=>{
+        for(let i =0;i<data.result.length;i++) {
+          if(user.user==data.result[i].id){
+            setUsername(data.result[i].username)
+            setBirthday(data.result[i].birthday)
+            setAddress(data.result[i].defaultAddress)
+            setNumber(data.result[i].phoneNumber)
+            setCC(data.result[i].defaultCreditCard)
+            setFetchedpass(data.result[i].password)
+          }
+        }
+      })
+    
+    },[])
+    
+    const update = () => {
       fetch("http://localhost:9000/shopping/usernames")
       .then((res) => res.json())
       .then((data)=>{
         for(let i =0;i<data.result.length;i++) {
           if(user.user==data.result[i].id){
+            setUsername(data.result[i].username)
             setBirthday(data.result[i].birthday)
             setAddress(data.result[i].defaultAddress)
             setNumber(data.result[i].phoneNumber)
             setCC(data.result[i].defaultCreditCard)
-
+            setFetchedpass(data.result[i].password)
           }
         }
-      })},[])
-    
-    const editonClick=()=>{
-      console.log("edit clicked")
+      })
+    };
+
+    const changepass=()=>{
+      setPassModel(true)
+    }
+    const closepass=()=>{
+      setPassModel(false)
+      update();
+    }
+    const changeuser=()=>{
+      setUserModel(true)
+    }
+    const closeuser=()=>{
+      setUserModel(false)
+      update();
+    }
+    const changebday=()=>{
+      setbdayModel(true)
+    }
+    const closebday=()=>{
+      setbdayModel(false)
+      update();
+    } 
+    const changeaddress=()=>{
+      setaddModel(true)
+    }
+    const closeaddress=()=>{
+      setaddModel(false)
+      update();
+    }
+    const changenumber=()=>{
+      setnumModel(true)
+    }
+    const closenumber=()=>{
+      setnumModel(false)
+      update();
+    }
+    const updatepass=()=>{
+      if(fetchedpass===curpass){
+        if(newpass===newconpass){
+        fetch("/account/changepassword?id="+user.user, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            pass: newpass,
+          }),
+        })
+      }
+      else{
+        setpasserror(true)
+      }
+ }    
+      else{
+        seterrorModel(true)
+      }
+    }
+    const updateusername=()=>{
+      fetch("/account/changeusername?id="+user.user, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+           username: newusername,
+        }),
+      })
+    }
+    const updatebday=()=>{
+      fetch("/account/changebday?id="+user.user, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          newbirthday: newbday,
+        }),
+      })
+    }
+
+    const updatenum=()=>{
+       fetch("/account/changenumber?id="+user.user, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          newNumber: newnum,
+        }),
+      })
+    }
+    const updateadd=()=>{
+      fetch("/account/changeaddress?id="+user.user, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          newadd: newadd,
+        }),
+      })
+    }
+    const closeerror=()=>{
+      seterrorModel(false)
+    }
+    const closepasserror=()=>{
+      setpasserror(false)
     }
     return(
       <>
       
       <Navbar ispage={[false, false, true]}/>
-          
-       <Grid container alignItems="center" justifyContent="center">
-          <Grid  marginTop='2vh'><Typography style={{fontFamily: "open sans"}}variant='h3'><b>Welcome {user.user}</b></Typography></Grid></Grid> 
+      
+      <Box sx={{width:'50%', alignitems:'center' }} p={5} px='25%'>
+        <Paper>
+          <Box p={5} textAlign='center' display='inline-block' alignItems='center'>
+          <Grid  marginTop='2vh'><Typography style={{fontFamily: "open sans"}}variant='h3'><b>Welcome {username}</b></Typography></Grid>
           <Grid item id="profileImage">{user.user[0]}</Grid>   
-          <Grid container alignItems="center" justifyContent="center" marginLeft='40%'> <Grid item xs={12}marginTop='2vh'><Typography variant='h6'style={{fontFamily: "open sans"}}><b>Birthday:</b> {birthday}</Typography></Grid>
-          <Grid item xs={12}marginTop='2vh'><Typography variant='h6'style={{fontFamily: "open sans"}}><b> Address:</b> {address}</Typography></Grid>
-          <Grid item xs={12}marginTop='2vh'><Typography variant='h6'style={{fontFamily: "open sans"}}> <b>Phone Number:</b> {number}</Typography></Grid>
-          <Grid item xs={12}marginTop='2vh'> <Typography variant='h6'style={{fontFamily: "open sans"}}> <b>Credit Card:</b> {cc}</Typography></Grid>
+          <Grid container alignItems="center" justifyContent="center" > <Grid item xs={12}marginTop='2vh'><Typography variant='h6'style={{fontFamily: "open sans"}}> <b>Birthday:</b> {birthday}       <EditIcon onClick={changebday}></EditIcon></Typography></Grid>
+          <Grid item xs={12}marginTop='2vh'><Typography variant='h6'style={{fontFamily: "open sans"}}><b> Address:</b> {address}       <EditIcon onClick={changeaddress}></EditIcon></Typography></Grid>
+
+          <Grid item xs={12}marginTop='2vh'><Typography variant='h6'style={{fontFamily: "open sans"}}> <b>Phone Number:</b> {number}       <EditIcon onClick={changenumber}></EditIcon></Typography></Grid>
        
-          <Grid item xs={12}marginTop='2vh'><Button style={{ backgroundColor: "#5BAFFF", fontFamily:'open sans' }} variant="contained" size='small'>Change Payment Method</Button></Grid>
+          <Grid item xs={12}marginTop='2vh'><Button style={{ backgroundColor: "#5BAFFF", fontFamily:'open sans' }} variant="contained" size='small' onClick={changepass}>Change Passsword</Button></Grid>
+          <Grid item xs={12}marginTop='2vh'><Button style={{ backgroundColor: "#5BAFFF", fontFamily:'open sans' }} variant="contained" size='small' onClick={changeuser}>Change Username</Button></Grid>
           <br></br>
           <br></br>
-          <Grid item xs={12}marginTop='2vh'> <Button  style={{ backgroundColor: "#5BAFFF", fontFamily:'open sans' }}variant="contained" size='small'onClick='editonClick'>Edit profile</Button></Grid>
-          </Grid> 
-      </>
+          </Grid> </Box></Paper></Box>
+          <Dialog open={passmodel}>
+          <Grid item marginLeft='92%' marginTop='2%'><ClearIcon onClick={closepass}></ClearIcon></Grid>
+            <DialogTitle variant="h4"><b>Change Password</b></DialogTitle>
+            <DialogContent>
+              <Grid item maxWidth='100%' marginLeft='12%'><TextField type='password'placeholder="Enter Current Password" onChange={(e)=>{setCurPass(e.target.value)}}></TextField></Grid>
+              <br/>
+              <Grid item marginLeft='12%'><TextField type='password' placeholder="Enter New Password" onChange={(e)=>{setNewPass(e.target.value)}}></TextField></Grid>
+              <br/>
+
+              <Grid item marginLeft='12%'> <TextField type='password'placeholder="Confirm New Password"onChange={(e)=>{setNewConPass(e.target.value)}}></TextField></Grid>
+              <br/>
+              <Grid item marginLeft='20%'><Button variant="contained"style={{ backgroundColor: "#5BAFFF", fontFamily:'open sans' }} onClick={(e)=>{updatepass();closepass(e)}}>Change Passsword</Button></Grid>
+              </DialogContent>
+          </Dialog>
+          <Dialog open={bdaymodel}>
+          <Grid item marginLeft='92%' marginTop='2%'><ClearIcon onClick={closebday}></ClearIcon></Grid>
+            <DialogTitle variant="h4"><b>Change Birthday</b></DialogTitle>
+            <DialogContent>
+              <Grid item maxWidth='100%' marginLeft='12%'><TextField placeholder="Enter New Birthday" onChange={(e)=>{setNewBday(e.target.value)}}></TextField></Grid>
+              <br/>
+              <Grid item marginLeft='20%'><Button variant="contained"style={{ backgroundColor: "#5BAFFF", fontFamily:'open sans' }} onClick={(e)=>{updatebday();closebday(e)}}>Change bday</Button></Grid>
+              </DialogContent>
+          </Dialog>
+          <Dialog open={nummodel}>
+          <Grid item marginLeft='92%' marginTop='2%'><ClearIcon onClick={closenumber}></ClearIcon></Grid>
+            <DialogTitle variant="h4"><b>Change Number</b></DialogTitle>
+            <DialogContent>
+              <Grid item maxWidth='100%' marginLeft='12%'><TextField placeholder="Enter New Number" onChange={(e)=>{setNewNum(e.target.value)}}></TextField></Grid>
+              <br/>
+              <Grid item marginLeft='20%'><Button variant="contained"style={{ backgroundColor: "#5BAFFF", fontFamily:'open sans' }} onClick={(e)=>{updatenum();closenumber(e)}}>Change number</Button></Grid>
+              </DialogContent>
+          </Dialog> <Dialog open={addmodel}>
+          <Grid item marginLeft='92%' marginTop='2%'><ClearIcon onClick={closeaddress}></ClearIcon></Grid>
+            <DialogTitle variant="h4"><b>Change Address</b></DialogTitle>
+            <DialogContent>
+              <Grid item maxWidth='100%' marginLeft='12%'><TextField placeholder="Enter New Address" onChange={(e)=>{setNewAdd(e.target.value)}}></TextField></Grid>
+              <br/>
+              <Grid item marginLeft='20%'><Button variant="contained"style={{ backgroundColor: "#5BAFFF", fontFamily:'open sans' }} onClick={(e)=>{updateadd();closeaddress(e)}}>Change Address</Button></Grid>
+              </DialogContent>
+          </Dialog>
+          <Dialog open={usernamemodel}>
+          <Grid item marginLeft='92%' marginTop='2%'><ClearIcon onClick={closeuser}></ClearIcon></Grid>
+            <DialogTitle variant="h4"><b>Change Username</b></DialogTitle>
+            <DialogContent>
+              <Grid item maxWidth='100%' marginLeft='12%'><TextField placeholder="Enter New Username" onChange={(e)=>{setNewUserName(e.target.value)}}></TextField></Grid>
+              <br/>
+              <Grid item marginLeft='20%'><Button variant="contained"style={{ backgroundColor: "#5BAFFF", fontFamily:'open sans' }} onClick={(e)=>{updateusername();closeuser(e)}}>Change Username</Button></Grid>
+              </DialogContent>
+          </Dialog>
+          <Dialog open={errormodel}>
+          <Grid item marginLeft='92%' marginTop='2%'><ClearIcon onClick={closeerror}></ClearIcon></Grid>
+          <DialogTitle>Error</DialogTitle>
+            <DialogContent sx={{color:'red'}}>Please enter the correct current password</DialogContent>
+          </Dialog>
+          <Dialog open={passerror}>
+          <Grid item marginLeft='92%' marginTop='2%'><ClearIcon onClick={closepasserror}></ClearIcon></Grid>
+          <DialogTitle>Error</DialogTitle>
+            <DialogContent sx={{color:'red'}}>Please confirm your new password correctly</DialogContent>
+          </Dialog>
+            </>
 )};
 
 
--- frontend/src/components/Shopping/Shopping.js
+++ frontend/src/components/Shopping/Shopping.js
@@ -1,17 +1,5 @@
 import Navbar from "../Navbar/Navbar";
-import {
-  Grid,
-  InputBase,
-  IconButton,
-  Card,
-  Stack,
-  Typography,
-  Button,
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem,
-} from "@mui/material";
+import { Grid,InputBase,IconButton,Card,Stack,Typography,Button,FormControl,InputLabel,Select,MenuItem,speedDialIconClasses} from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import React, { useEffect, useState, useRef } from "react";
 import Product from "./Product";
@@ -23,7 +11,12 @@ const stack1 = [];
 const stack2 = [];
 const stack3 = [];
 const stack4 = [];
-let object = null;
+const categorizedStack1 = [];	
+const categorizedStack2 = [];	
+const categorizedStack3 = [];	
+const categorizedStack4 = [];	
+let object = null;	
+let categorized = false;
 
 const Homepage = () => {
   const [stateStack1, setStateStack1] = useState([]);
@@ -142,6 +135,62 @@ const Homepage = () => {
     const searchStack2 = [];
     const searchStack3 = [];
     const searchStack4 = [];
+    if (categorized) {	
+      for (let i = 0; i < categorizedStack1.length; i++) {	
+        if (categorizedStack1[i].name.includes(textFieldRefSearch.current.value)) {	
+          searchedFor.push(categorizedStack1[i]);	
+        }	
+      }	
+      for (let i = 0; i < categorizedStack2.length; i++) {	
+        if (categorizedStack2[i].name.includes(textFieldRefSearch.current.value)) {	
+          searchedFor.push(categorizedStack2[i]);	
+        }	
+      }	
+      for (let i = 0; i < categorizedStack3.length; i++) {	
+        if (categorizedStack3[i].name.includes(textFieldRefSearch.current.value)) {	
+          searchedFor.push(categorizedStack3[i]);	
+        }	
+      }	
+      for (let i = 0; i < categorizedStack4.length; i++) {	
+        if (categorizedStack4[i].name.includes(textFieldRefSearch.current.value)) {	
+          searchedFor.push(categorizedStack4[i]);	
+        }	
+      }	
+      let length = searchedFor.length;	
+      const stack4length = Math.floor(length / 4);	
+      length = length - stack4length;	
+      const stack3length = Math.floor(length / 3);	
+      length = length - stack3length;	
+      const stack2length = Math.floor(length / 2);	
+      length = length - stack2length;	
+      const stack1length = Math.floor(length / 1);	
+      length = length - stack1length;	
+      for (let i = 0; i < stack1length; i++) {	
+        searchStack1[i] = searchedFor[i];	
+      }	
+      for (let i = stack1length; i < stack2length + stack1length; i++) {	
+        searchStack2[i] = searchedFor[i];	
+      }	
+      for (	
+        let i = stack2length + stack1length;	
+        i < stack3length + stack2length + stack1length;	
+        i++	
+      ) {	
+        searchStack3[i] = searchedFor[i];	
+      }	
+      for (	
+        let i = stack3length + stack2length + stack1length;	
+        i < stack4length + stack3length + stack2length + stack1length;	
+        i++	
+      ) {	
+        searchStack4[i] = searchedFor[i];	
+      }	
+      setStateStack1(searchStack1);	
+      setStateStack2(searchStack2);	
+      setStateStack3(searchStack3);	
+      setStateStack4(searchStack4);	
+      return;	
+    }
     for (let i = 0; i < stack1.length; i++) {
       if (stack1[i].name.includes(textFieldRefSearch.current.value)) {
         searchedFor.push(stack1[i]);
@@ -198,7 +247,77 @@ const Homepage = () => {
   };
 
   const handleChange = (event) => {
-    setCategory(event.target.value);
+    if(event.target.value === "all") {	
+      setStateStack1(stack1);	
+      setStateStack2(stack2);	
+      setStateStack3(stack3);	
+      setStateStack4(stack4);	
+      categorized = false;	
+      textFieldRefSearch.current.value = "";	
+      setCategory(event.target.value);	
+      return;	
+  }	
+  const catFor = [];	
+  for (let i = 0; i < stack1.length; i++) {	
+    if (stack1[i].category === (event.target.value)) {	
+      catFor.push(stack1[i]);	
+    }	
+  }	
+  for (let i = 0; i < stack2.length; i++) {	
+    if (stack2[i].category === (event.target.value)) {	
+      catFor.push(stack2[i]);	
+    }	
+  }	
+  for (let i = 0; i < stack3.length; i++) {	
+    if (stack3[i].category === (event.target.value)) {	
+      catFor.push(stack3[i]);	
+    }	
+  }	
+  for (let i = 0; i < stack4.length; i++) {	
+    if (stack4[i].category === (event.target.value)) {	
+      catFor.push(stack4[i]);	
+    }	
+  }	
+  let length = catFor.length;	
+  const stack4length = Math.floor(length / 4);	
+  length = length - stack4length;	
+  const stack3length = Math.floor(length / 3);	
+  length = length - stack3length;	
+  const stack2length = Math.floor(length / 2);	
+  length = length - stack2length;	
+  const stack1length = Math.floor(length / 1);	
+  length = length - stack1length;	
+  categorizedStack1.splice(0, categorizedStack1.length);	
+  categorizedStack2.splice(0, categorizedStack2.length);	
+  categorizedStack3.splice(0, categorizedStack3.length);	
+  categorizedStack4.splice(0, categorizedStack4.length);	
+  for (let i = 0; i < stack1length; i++) {	
+    categorizedStack1.push(catFor[i]);	
+  }	
+  for (let i = stack1length; i < stack2length + stack1length; i++) {	
+    categorizedStack2.push(catFor[i]);	
+  }	
+  for (	
+    let i = stack2length + stack1length;	
+    i < stack3length + stack2length + stack1length;	
+    i++	
+  ) {	
+    categorizedStack3.push(catFor[i]);	
+  }	
+  for (	
+    let i = stack3length + stack2length + stack1length;	
+    i < stack4length + stack3length + stack2length + stack1length;	
+    i++	
+  ) {	
+    categorizedStack4.push(catFor[i]);	
+  }	
+  categorized = true;	
+  textFieldRefSearch.current.value = "";	
+  setStateStack1(categorizedStack1);	
+  setStateStack2(categorizedStack2);	
+  setStateStack3(categorizedStack3);	
+  setStateStack4(categorizedStack4);	
+  setCategory(event.target.value);	
   };
 
   const toCart = (priceID, price, name) => {
@@ -240,7 +359,7 @@ const Homepage = () => {
       {!productSelected && (
         <Grid container justifyContent="center" marginTop="1rem">
           <Grid item xs={12}>
-            <Card sx={{ width: "15%", marginLeft: "42.5%" }}>
+            <Card sx={{ width: "15%", marginLeft: "42.5%", backgroundcolor: "transparent" }}>
               <InputBase
                 sx={{ ml: 1, flex: 1, width: "81%" }}
                 placeholder="Search Products"
@@ -258,13 +377,14 @@ const Homepage = () => {
             </Card>
           </Grid>
           <Grid item xs={2}>
-            <FormControl fullWidth>
+            <FormControl style={{ marginLeft: "10%",width: "80%", marginTop: "2rem"}}>
               <InputLabel>Category</InputLabel>
               <Select
                 value={category}
                 label="Category"
                 onChange={handleChange}
               >
+                <MenuItem value={'all'}>All Departments</MenuItem>
                 <MenuItem value={'men pants'}>Men's Pants</MenuItem>
                 <MenuItem value={'dress'}>Women's Dress</MenuItem>
                 <MenuItem value={'men shorts'}>Men's Shorts</MenuItem>
@@ -344,16 +464,24 @@ const Homepage = () => {
         </Grid>
       )}
       {productSelected && (
-        <Grid container>
-          <Grid item>
-            <img src={object.img}/>
-          </Grid>
-          <Grid item>
-            <Stack>
-              <Typography variant="h6">{object.name}</Typography>
-              <Typography variant="h3">US: ${object.price}</Typography>
-              <Typography variant="h6">Likes:{object.likes}</Typography>
-              <Typography variant="h6">
+        	        <>	
+                  <Grid container spacing={3}>	
+                  <Grid item sx={6}>
+                  <Card style={{backgroundColor:"white",maxHeight:"80vh",width:"50vh", marginLeft:"100px", marginTop:"25px" }}>	
+                    <img src={object.img} style={{height:"700px",width:"400px"}}/>	
+                  </Card>	
+                  </Grid>
+                  <Card style={{backgroundColor:"white",height:"400px",width:"500px", textalign:"right", marginTop:"100px", marginbottom:"200px", marginLeft:"300px", paddingLeft:"20px", paddingTop:"20px", paddingRight:"20px"}}>	
+                    <Stack style={{backgroundcolor:"transparent"}}>	
+                      <Typography style={{textAlign:"center", fontSize:"xx-large", fontstyle:"Times New Roman"}} variant="h1">{object.name}</Typography>	
+                      <br></br>	
+                      <Typography  style={{textAlign:"center"}}variant="p">{object.bio}</Typography>	
+                      <br>	
+                      </br>	
+                      <Typography  style={{textAlign:"center", fontSize:"x-large"}} variant="h3">US: ${object.price}</Typography>	
+                        <br></br>	
+                      <Typography  style={{textAlign:"center"}} variant="h6">Likes:{object.likes}</Typography>	
+                      <Typography  style={{textAlign:"center"}} variant="h6">
                 Do you like this product?
                 {!liked && (
                   <IconButton
@@ -376,19 +504,18 @@ const Homepage = () => {
                   </IconButton>
                 )}
               </Typography>
-              <Typography variant="p">{object.bio}</Typography>
-              <Button onClick={() => toCart(object.priceID, object.name, object.price)}>Add to Cart</Button>
-              <Button>Buy Now</Button>
-              <Typography variant="h5">
+              <Typography style={{textAlign:"center"}} variant="h5">
                 Shipping Cost: ${object.shippingCost}
               </Typography>
-              <Typography variant="h5">
+              <Typography style={{textAlign:"center"}} variant="h5">
                 Estimated Delivery Time: {object.shippingTime}
               </Typography>
+              <Button variant="contained" onClick={() => toCart(object.priceID, object.name, object.price)}>Add to Cart</Button>
               <Button onClick={backToShopping}>Go Back</Button>
             </Stack>
+            </Card>
           </Grid>
-        </Grid>
+        </>
       )}
     </>
   );
